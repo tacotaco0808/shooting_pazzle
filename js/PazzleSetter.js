94764fe3c08890940c2edf94a32b1ab962255623
takutou <takuto1488@gmail.com>
2023-12-12 14:54:33 +0900

    オブジェクトのbody表示・非表示成功

--- js/PazzleSetter.js
+++ js/PazzleSetter.js
@@ -23,18 +23,21 @@ class PazzleSetter extends Phaser.Physics.Arcade.Sprite {
     super.preUpdate(time, delta);
     //表示されていなかったら表示する
     if (!this.visible && time > this.lastCollision) {
+      this.scene.physics.world.enable(this); //bodyの表示
       this.setVisible(true);
     }
 
-    //衝突判定
+    //衝突判定 触れてから5000ms後にもう一度触れれる
     if (this.scene.physics.overlap(this.player, this) && time > this.lastCollision) {
       console.log('SetPazzle');
       this.pazzlePlate.shuffleAndSetImage(this.holdPazzleNum); //自分で作ったクラスのインスタンスメソッド
+      this.scene.physics.world.disable(this); //bodyの非表示
       this.setVisible(false);
       this.lastCollision = time + 5000;
     }
   }
   setHoldPazzleNum(pazzleNum) {
+    //表示させるパズルの個数を親クラスからPazzleSetterへ渡す
     this.holdPazzleNum = pazzleNum;
   }
 }
